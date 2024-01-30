beedc1347fb10dd49b60c042552a548889e3ed69
Kenny-still12 <kensonoda1124@gmail.com>
2024-01-30 13:41:37 +0800

    Added basic variables

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const numBtns = document.querySelectorAll('.number');
+const operatorBt = document.querySelectorAll('.operation');
+const clearBtn = document.querySelectorAll('.clear');
+const percentBtn = document.querySelectorAll('.percent');
+const backBtn = document.querySelectorAll('.back');
+const deciamalBtn = document.querySelectorAll('.decimal');
+const equalBtn = document.querySelectorAll('.equals');
+const result = document.querySelector('.display');
+
+let fisrtNumber = '';
+let secondNumber = '';
+let displayedNumber = '';
+let operator = '';
+
+
 function add (a,b){
     return a + b;
 }
